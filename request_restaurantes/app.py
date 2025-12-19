c81ad46661170ae50b7953f69f4c32294daa4a4c
Thiago <thiagorovari@gmail.com>
2025-12-19 17:32:13 -0300

    Refactor app.py to handle restaurant data response and improve error handling

--- request_restaurantes/app.py
+++ request_restaurantes/app.py
@@ -4,4 +4,22 @@ url =  'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
 
 response = requests.get(url)
 
-print(response)
+#print(response)
+
+if response.status_code == 200:
+    dados_json = response.json()
+    dados_restaurante = {}
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+        
+        dados_restaurante[nome_do_restaurante].append({
+            "item": item['Item'],
+            "price": item['price'],
+            "description": item['description']
+        })
+else:
+    print(f"Erro ao receber dados")
+
+print(dados_restaurante['McDonald’s'])
